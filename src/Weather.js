83220044534215975cda3b121063a4a709cf57a9
TetGar <109559114+TetGar@users.noreply.github.com>
2023-01-19 18:00:16 +0000

    Added useState varible

--- src/Weather.js
+++ src/Weather.js
@@ -1,54 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
+  const [weatherInfo, setWeatherInfo] = useState({ run: false });
+
   function showTemperature(response) {
-    console.log(response.data);
+    setWeatherInfo({
+      run: true,
+      temperature: Math.round(response.data.main.temp),
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      wind: Math.round(response.data.wind.speed),
+      city: response.data.name,
+      icon: "",
+      date: "Wednesday 20:00",
+    });
   }
 
-  const apiKey = "50c2acd53349fabd54f52b93c8650d37";
-  let city = "London";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  axios.get(apiUrl).then(showTemperature);
-
-  return (
-    <div className="Weather p-4">
-      <form className="mb-2">
+  if (weatherInfo.run) {
+    return (
+      <div className="Weather p-4">
+        <form className="mb-2">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />{" "}
+            </div>
+          </div>
+        </form>
+        <h1>London</h1>
+        <ul>
+          <li>{weatherInfo.date}</li>
+          <li className="text-capitalize">{weatherInfo.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="party cloudy"
             />
+            <span className="temperature"> {weatherInfo.temperature}</span>
+            <span className="units">°C</span>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />{" "}
+          <div className="col-6">
+            <ul>
+              <li>Humidity:{weatherInfo.humidity}%</li>
+              <li>Wind: {weatherInfo.wind} km/h</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h1>London</h1>
-      <ul>
-        <li>Tuesday 20:00</li>
-        <li>Clouds</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="party cloudy"
-          />
-          <span className="temperature"> 7</span>
-          <span className="units">°C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity:70%</li>
-            <li>Wind: 15 km/h</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "5293d8454b519c30f6f6331f38c85b4c";
+    let city = "London";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemperature);
+
+    return "Loading...";
+  }
 }
